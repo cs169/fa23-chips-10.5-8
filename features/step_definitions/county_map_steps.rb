31cfeb6b8dd91855e10241a25c870878883f7bf0
Tracy Ye <yye029@berkeley.edu>
2023-11-23 22:35:00 +0000

    have to change the route linked to view button to pass the cucumber workaround tests

--- features/step_definitions/county_map_steps.rb
+++ features/step_definitions/county_map_steps.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
-Given(/^I navigate to the "([A-Z]{2})" state map page$/) do |state|
-  visit "/state/#{state}"
-  print(page.body)
+Given(/^I navigate to the "([A-Z]{2})" state map page$/) do |state_name|
+  visit "/state/#{state_name}"
 end
 
-When('I click on the "Counties in California" button') do
-  click_button('Counties in California')
+When('I click on the Counties in {string} button') do |state_name|
+  click_button("Counties in #{state_name}")
 end
 
 Then('I should see a list of counties') do
@@ -15,6 +14,17 @@ Then('I should see a list of counties') do
   end
 end
 
-And('I can click on the "View" link for the county with FIPS code {string}') do |_fips_code|
-  find('a.btn.btn-primary[href="/state/CA/county/037"]').click
+When('I click on the "View" link for the county with county name {string}') do |county_name|
+  encoded_county_name = URI.encode_www_form_component(county_name).gsub('+', '%20')
+  find("a.btn.btn-primary[href=\"/search/#{encoded_county_name}\"]").click
+end
+
+Then('I should be taken to the search page for county {string}') do |county_name|
+  encoded_county_name = URI.encode_www_form_component(county_name).gsub('+', '%20')
+  expect(page).to have_current_path("/search/#{encoded_county_name}")
+end
+
+And('I should see a list of representatives') do
+  # Check that the page has content listing the representatives for the county
+  expect(page).to have_content('Joseph R. Biden')
 end
