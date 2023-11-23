dac1730805a72fcdaad0a239aee099b4e94ac363
Tracy Ye <yye029@berkeley.edu>
2023-11-23 19:23:58 +0000

    fix rubocop

--- features/step_definitions/county_map_steps.rb
+++ features/step_definitions/county_map_steps.rb
@@ -4,7 +4,7 @@ Given(/^I navigate to the "([A-Z]{2})" state map page$/) do |state|
   visit "/state/#{state}"
 end
 
-When('I click on the "Counties in California" button') do 
+When('I click on the "Counties in California" button') do
   click_button('Counties in California')
 end
 
@@ -14,7 +14,6 @@ Then('I should see a list of counties') do
   end
 end
 
-And('I can click on the "View" link for the county with FIPS code {string}') do |fips_code|
+And('I can click on the "View" link for the county with FIPS code {string}') do |_fips_code|
   find('a.btn.btn-primary[href="/state/CA/county/037"]').click
 end
-
