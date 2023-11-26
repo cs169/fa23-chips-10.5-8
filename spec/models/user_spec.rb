47c090a20a1345afcd080db562ad54d1c9795695
Tracy Ye <yye029@berkeley.edu>
2023-11-26 06:12:51 +0000

    [refactor]refactor code duplication for user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,36 +4,38 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'methods' do
+    let!(:google_user) do
+      described_class.create(provider: :google_oauth2, uid: '001', first_name: 'Brevin', last_name: 'Smider')
+    end
+    let!(:github_user) do
+      described_class.create(provider: :github, uid: '001', first_name: 'Brevin', last_name: 'Smider')
+    end
+
     describe '#name' do
       it 'returns a users name' do
-        u = described_class.create(first_name: 'Brevin', last_name: 'Smider', provider: :google_oauth2, uid: '001')
-        expect(u.name).to eq('Brevin Smider')
+        expect(google_user.name).to eq('Brevin Smider')
       end
     end
 
     describe '#auth_provider' do
-      it 'testing that a users authentication provider is google' do
-        u = described_class.create(provider: :google_oauth2, uid: '001')
-        expect(u.auth_provider).to eq('Google')
+      it 'returns Google as the auth provider' do
+        expect(google_user.auth_provider).to eq('Google')
       end
 
-      it 'testing that a users authentication provider is github' do
-        u = described_class.create(provider: :github, uid: '001')
-        expect(u.auth_provider).to eq('Github')
+      it 'returns Github as the auth provider' do
+        expect(github_user.auth_provider).to eq('Github')
       end
     end
 
     describe '.find_google_user' do
       it 'returns a user found through a google UID' do
-        u = described_class.create(provider: :google_oauth2, uid: '001')
-        expect(described_class.find_google_user('001')).to eq(u)
+        expect(described_class.find_google_user('001')).to eq(google_user)
       end
     end
 
     describe '.find_github_user' do
       it 'returns a user found through a github UID' do
-        u = described_class.create(provider: :github, uid: '001')
-        expect(described_class.find_github_user('001')).to eq(u)
+        expect(described_class.find_github_user('001')).to eq(github_user)
       end
     end
   end
