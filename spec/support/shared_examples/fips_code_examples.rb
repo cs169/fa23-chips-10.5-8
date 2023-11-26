88bbb10672b06e2c63e59050994203241d6d9a59
Tracy Ye <yye029@berkeley.edu>
2023-11-26 07:14:53 +0000

    [refactor]refactor state_spec.rb and county_spec.rb

--- spec/support/shared_examples/fips_code_examples.rb
+++ spec/support/shared_examples/fips_code_examples.rb
@@ -1,20 +1,14 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples 'a model of state or county' do |model, padding|
-  it 'returns a FIPS from a single digit' do
-    instance = model.new(fips_code: 9)
-    expected_code = '9'.rjust(padding, '0')
-    expect(instance.std_fips_code).to eq(expected_code)
-  end
-
-  it 'returns a FIPS from double digits' do
-    instance = model.new(fips_code: 99)
-    expected_code = '99'.rjust(padding, '0')
-    expect(instance.std_fips_code).to eq(expected_code)
-  end
-
-  it 'returns a FIPS from triple digits' do
-    instance = model.new(fips_code: 999)
-    expect(instance.std_fips_code).to eq('999')
+  [
+    [9, '9'.rjust(padding, '0')],
+    [99, '99'.rjust(padding, '0')],
+    [999, '999']
+  ].each do |fips_code, expected_code|
+    it "returns the correct FIPS code for #{fips_code}" do
+      new_instance = model.new(fips_code: fips_code)
+      expect(new_instance.std_fips_code).to eq(expected_code)
+    end
   end
 end
