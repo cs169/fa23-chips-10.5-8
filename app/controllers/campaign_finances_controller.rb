8ae971565a7ac4293736949cbf4dfde36b08e125
Tracy Ye <yye029@berkeley.edu>
2023-11-29 23:53:55 +0000

    [feat]create basic controller and a view folder

---
+++ app/controllers/campaign_finances_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class CampaignFinancesController < ApplicationController
+  def index
+    # Action for listing campaign finance data
+  end
+
+  def show
+    # Action for showing details of a specific campaign finance record
+  end
+
+  def search
+    # Action to handle search functionality
+  end
+
+  # Any private methods needed for this controller
+end
