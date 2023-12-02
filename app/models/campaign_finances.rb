339d5539222eeabb96c300af8e36bc62f8d91a60
Tracy Ye <yye029@berkeley.edu>
2023-12-02 21:05:13 +0000

    [feat]implement model for campaign finances

--- app/models/campaign_finances.rb
+++ app/models/campaign_finances.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class CampaignFinances < ApplicationRecord
-  def self.find_in_propublica(cycle, category); end
+  def self.find_in_propublica(cycle, _category)
+    Faraday.get(cycle)
+  end
 end
